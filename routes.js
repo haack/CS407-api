e387e6c34519590e3676e33875d51dd7cdfbe18a
Alex <alex_haak@hotmail.co.uk>
2015-04-13 12:01:05 +0100

    Added basic infrastructure and api stub

---
+++ routes.js
@@ -0,0 +1,5 @@
+console.log("rootes");
+
+exports.test = function(req, res, next) {
+	res.send({'Yay':'This has gone swimmingly'});
+};
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("hello");
+var express = require('express'),
+	routes = require('./routes.js');
+
+console.log("Starting server...");
+var app = express();
+
+app.get('/test', routes.test);
+
+app.listen(1337);
+console.log('Listening on port 1337...');
