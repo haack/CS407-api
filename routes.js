e6916a07428ea0ed8671afef1d4dc30dd42fb6bc
Alex <alex_haak@hotmail.co.uk>
2015-04-16 22:41:32 +0100

    Added recommendations using euclidean distance

--- routes.js
+++ routes.js
@@ -5,18 +5,18 @@ db = mongojs.connect("fluctus", ["featuredata"]);
 exports.song = function(req, res) {
 	console.log("/GET on " + req.originalUrl);
 
-    var file = req.params.filename;
+	var file = req.params.filename;
 	console.log("Requesting: " + file);
 
 	db.featuredata.findOne({"name": file}, function(err, docs) {
-	    // docs is an array of all the documents in mycollection 
-	    console.log(docs);
-	    if (docs) {
-	    	docs.seen = true;
-	    	res.send(docs);
-	    } else {
-	    	res.send({"seen": false});
-	    }
+		// docs is an array of all the documents in mycollection 
+		console.log(docs);
+		if (docs) {
+			docs.seen = true;
+			res.send(docs);
+		} else {
+			res.send({"seen": false});
+		}
 	});
 };
 
@@ -29,6 +29,7 @@ exports.addSong = function(req, res) {
 	db.featuredata.insert(req.body, function(err, result) {
 		if (!err) {
 			res.send({'result': 'success'});
+			distance(req.body, null);
 		} else {
 			res.send({'result': 'error'});
 		}
@@ -40,11 +41,75 @@ exports.recommend = function(req, res) {
 
 	console.log("Generating recommendations...");
 
-	res.send({"recs": [
-			{"name": "Dark_side_of_the_moon"},
-			{"name": "Kitchen_sink"}
-		]});
+	clusters = req.body;
+	//TODO fetch feature data for each song
+	epsilon = 5;
+	//TODO send from client
+
+	db.featuredata.find(function(err, docs) {
+		targetSongs = getTargetSongs(docs, clusters);
+
+		if(targetSongs.length == 0) {
+			return "No recommendations available";
+		}
+
+		var recs = [];
+		
+		for(var i in targetSongs) {
+			var count = 0;
+
+			for(j in clusters) {
+				if(euclidean(targetSongs[i], clusters[j]) <= epsilon) {
+					count++;
+				}
+			}
+
+			if(((count > 0) && (recs.length < 10)) || ((recs.length > 10) && (count > recs[10].count))) {
+				recs.push({"song":targetSongs[i],"count":count});
+				recs.sort(function(a, b){return a.count - b.count});
+
+				if(recs.legth > 10){
+					recs.pop();
+				}
+			}
+		}
+
+		if(recs.length == 0){
+			res.send({"Result": "No recommendations available"});
+		}
+		else {
+			console.log(recs.length);
+			res.send(recs);
+		}
+	});
 };
+
+getTargetSongs = function(all, user) {
+	var targetSongs = [];
+	for (i in all) {
+		var match = false;
+		for (j in user) {
+			if (all[i].name == user[j].name) {
+				match = true;
+				break;
+			}
+		}
+		if (!match) {
+			targetSongs.push(all[i]);
+		}
+	}
+	return targetSongs;
+}
+
+euclidean = function(s1,s2) {
+	var sum = 0;
+	for(var i in s1.features) {
+		var temp = Math.abs(s1.features[i].value - s2.features[i].value);
+		sum += temp*temp;
+	}
+
+	return Math.sqrt(sum);
+}
 
 exports.testGet = function(req, res) {
 	console.log("/GET on /test");
